26a4c2a756f718a7638046bd0039e7cceef5a3d9
Marcosdev03 <sdfreitas7@gmail.com>
2026-05-22 22:02:17 -0300

    Endpoint database validado com sucesso

--- src/BancoDigital.Api/Endpoints/HealthEndpoints.cs
+++ src/BancoDigital.Api/Endpoints/HealthEndpoints.cs
@@ -1,3 +1,7 @@
+using BancoDigital.Api.Data;
+using Dapper;
+
+
 namespace BancoDigital.Api.Endpoints;
 
 public static class HealthEndpoints
@@ -5,7 +9,7 @@ public static class HealthEndpoints
     public static IEndpointRouteBuilder MapHealthEndpoints(this IEndpointRouteBuilder app)
     {
         var group = app.MapGroup("/health")
-        .WithTags("Health");
+            .WithTags("Health");
 
         group.MapGet("/", () =>
         {
@@ -13,15 +17,43 @@ public static class HealthEndpoints
             {
                 status = "healthy",
                 service = "BancoDigital.Api",
-                checkedAt = DateTime.UtcNow
+                checkedAt = DateTimeOffset.UtcNow
+            });
+        })
+        .WithName("GetHealth")
+        .WithOpenApi();
+
+        group.MapGet("/database", async (SqlConnectionFactory connectionFactory) =>
+        {
+            await using var connection = connectionFactory.Create();
+
+            var result = await connection.QuerySingleAsync<DatabaseHealthResult>(
+                """
+                SELECT 
+                    DB_NAME() AS DatabaseName,
+                    COUNT(*) AS TotalClientes
+                FROM dbo.Clientes;
+                """);
+
+            return Results.Ok(new
+            {
+                status = "healthy",
+                database = result.DatabaseName,
+                totalClientes = result.TotalClientes,
+                checkedAt = DateTimeOffset.UtcNow
 
             });
 
         })
-        .WithName("GetHealthStatus")
+        .WithName("GetDatabaseHealth")
         .WithOpenApi();
+
 
         return app;
 
-        }    
+    }
+
+    private sealed record DatabaseHealthResult(string DatabaseName, int TotalClientes);
+
 }
+
--- src/BancoDigital.Api/Program.cs
+++ src/BancoDigital.Api/Program.cs
@@ -1,11 +1,14 @@
 using BancoDigital.Api.Endpoints;
+using BancoDigital.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<SqlConnectionFactory>();
 
 var app = builder.Build();
 
